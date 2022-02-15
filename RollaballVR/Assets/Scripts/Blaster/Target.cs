e0f687b21e6dcaceaebb23799d974da41de8dae1
Ryan Schumacher <Schumacherryan2001@gmail.com>
2022-02-15 15:01:55 -0600

    Have gun functionality implemented, have not yet tested

--- RollaballVR/Assets/Scripts/Blaster/Target.cs
+++ RollaballVR/Assets/Scripts/Blaster/Target.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Color m_FlashDamageColor = Color.white;
+
+    private MeshRenderer m_MeshRenderer = null;
+    private Color m_OriginalColor = Color.white;
+
+    private int m_MaxHealth = 2;
+    private int m_Health = 0;
+
+    private void Awake()
     {
-        
+        m_MeshRenderer = GetComponent<MeshRenderer>();
+        m_OriginalColor = m_MeshRenderer.material.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        ResetHealth();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {   
+        if(collision.gameObject.CompareTag("Projectile"))
+            Damage();
+    }
+
+    private void Damage()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Flash());
+
+        RemoveHealth();
+    }
+
+    private IEnumerator Flash()
+    {
+        m_MeshRenderer.material.color = m_FlashDamageColor;
+        WaitForSeconds wait = new WaitForSeconds(.1f);
+        yield return wait;
+        m_MeshRenderer.material.color = m_OriginalColor;
+    }
+
+    private void RemoveHealth()
+    {
+        m_Health--;
+        CheckForDeath();
+    }
+
+    private void ResetHealth()
+    {
+        m_Health = m_MaxHealth;
+    }
+
+    private void CheckForDeath()
+    {
+        if(m_Health <= 0)
+        {
+            KillTarget();
+        }
+    }
+
+    private void KillTarget()
+    {
+        gameObject.SetActive(false);
     }
 }
